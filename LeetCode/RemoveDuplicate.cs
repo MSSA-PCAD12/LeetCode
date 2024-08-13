e33acf2a9003e2ccfa3045403953d2f6e9b43a17
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-13 13:42:57 -0700

    finished remove duplicate

---
+++ LeetCode/RemoveDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class RemoveDuplicate
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int result = 0, pointer = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                if (i == 0) { result++; continue; }
+                if (nums[i] != nums[i - 1]) {
+                    result++;
+                    pointer++;
+                    nums[pointer] =nums[i];
+                }
+            }
+            return result;
+        }
+    }
+}
