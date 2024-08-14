b7393b567267ec082827a5754478d95a76297359
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-14 10:10:19 -0700

    Roman to Int

---
+++ LeetCode/RomanNumeral.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{//https://leetcode.com/problems/roman-to-integer/
+    public class RomanNumeral
+    {
+        public int RomanToInt(string s)
+        {
+            int result=0;
+            Dictionary<string, int> lookup = new Dictionary<string, int>() {
+            { "I",1 },
+            { "V",5 },
+            { "X",10 },
+            { "L",50 },
+            { "C",100 },
+            { "D",500 },
+            { "M",1000 },
+            { "IV",4 },
+            { "IX",9 },
+            { "XL",40 },
+            { "XC",90 },
+            { "CD",400 },
+            { "CM",900 }
+            };
+            for (int i = 0; i < s.Length; i++)
+                if (i + 1 < s.Length)
+                {
+                    if (lookup.ContainsKey(string.Concat(s[i], s[i + 1])))
+                    {
+                        result += lookup[string.Concat(s[i], s[i + 1])];
+                        i += 1;
+                        continue;
+                    }
+                    result += lookup[s[i].ToString()];
+                }
+                else
+                { result += lookup[s[i].ToString()]; }
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/RomanNumeralTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass]
+    public class RomanNumeralTests
+    {
+        [TestMethod()]
+        public void RomanToIntTest()
+        {
+            string s = "III";
+            RomanNumeral r = new();
+            int expected = 3;
+            int actual= r.RomanToInt(s);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void RomanToIntTest2()
+        {
+            string s = "LVIII";
+            RomanNumeral r = new();
+            int expected = 58;
+            int actual = r.RomanToInt(s);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void RomanToIntTest3()
+        {
+            string s = "MCMXCIV";
+            RomanNumeral r = new();
+            int expected = 1994;
+            int actual = r.RomanToInt(s);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
