8276b3173c866f75441b093f90bc143a16f4083f
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-14 09:00:05 -0700

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock

---
+++ LeetCode/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+    public class BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int largestProfit = 0;
+            int lowestPriceSofar = int.MaxValue;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < lowestPriceSofar) // is this a new low price so far? 
+                {
+                    lowestPriceSofar = prices[i];// update the lowestPriceSoFar
+                }
+                else 
+                {
+                    largestProfit = Math.Max(largestProfit, prices[i]-lowestPriceSofar);//price is higher, is the profit the highest we have seen?
+                }
+
+            }
+
+            return largestProfit;
+        }
+    }
+}
---
+++ LeetCodeTests/BestTimeToBuyAndSellStockTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class BestTimeToBuyAndSellStockTests
+    {
+        [TestMethod()]
+        public void MaxProfitTest()
+        {
+            int[] prices = [7, 1, 5, 3, 6, 4];
+            int expected = 5;
+            BestTimeToBuyAndSellStock b = new();
+            int actual = b.MaxProfit(prices);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxProfitTest2()
+        {
+            int[] prices = [7, 6, 4, 3, 1];
+            int expected = 0;
+            BestTimeToBuyAndSellStock b = new();
+            int actual = b.MaxProfit(prices);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
