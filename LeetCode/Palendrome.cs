35e599eb1d0e9dab9a5f4930ba20e30264d410dc
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-14 15:00:15 -0700

    Palindrome https://leetcode.com/problems/valid-palindrome/

---
+++ LeetCode/Palendrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    //https://leetcode.com/problems/valid-palindrome/
+    public class Palendrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            int leftPointer = 0;
+            int rightPointer = s.Length - 1;
+
+            while (leftPointer < rightPointer)
+            {
+                while (!(char.IsLetterOrDigit(s[leftPointer]) && char.IsLetterOrDigit(s[rightPointer])))
+                {
+                    leftPointer = char.IsLetterOrDigit(s[leftPointer]) ? leftPointer : ++leftPointer;
+                    rightPointer = char.IsLetterOrDigit(s[rightPointer]) ? rightPointer : --rightPointer;
+                    if (leftPointer >= s.Length - 1 || rightPointer <= 0) { return true; }
+                }
+                if (char.ToLower(s[leftPointer]) == char.ToLower(s[rightPointer]))
+                {
+                    leftPointer++;
+                    rightPointer--;
+                }
+                else
+                { 
+                    return false; 
+                }
+
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCodeTests/PalendromeTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class PalendromeTests
+    {
+        [TestMethod()]
+        public void IsPalindromeTest()
+        {
+            string s = "A man, a plan, a canal: Panama";
+            bool expected = true;
+            Palendrome p = new();
+            bool actual =p.IsPalindrome(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsPalindromeTest2()
+        {
+            string s = "race a car";
+            bool expected = false;
+            Palendrome p = new();
+            bool actual = p.IsPalindrome(s);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
