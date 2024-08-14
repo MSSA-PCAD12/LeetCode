bfad284f4180f69784be4d905a9d444a50097270
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-14 15:12:56 -0700

    remove-duplicates-from-sorted-array-ii

--- LeetCode/ArrayString/RemoveDuplicateII.cs
+++ LeetCode/ArrayString/RemoveDuplicateII.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LeetCode
-{
+{//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii
     public class RemoveDuplicateII
     {
         public int RemoveDuplicates(int[] nums)
         {
-            int result = 0, pointer = 0,currentNumberCount = 0;
-            for (int i = 0; i < nums.Length; i++) {
-                if (i == 0) { result++; continue; }
-                while (nums[i] == nums[i + 1]) {
-                    currentNumberCount++;
-                    i++;
+            int counter = 0;
+            bool hasAppearedTwoTimes=false;
 
-                    
+            for (int i = 1; i < nums.Length; i++) { 
+            bool isRepeat = (nums[counter] == nums[i]);
+                if (!isRepeat || !hasAppearedTwoTimes)
+                {
+                    nums[++counter] = nums[i];
                 }
+                hasAppearedTwoTimes = isRepeat;
             }
-            return result;
+
+            return counter+1;
         }
     }
 }
