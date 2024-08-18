eaaebd0509a5f2eac09b1529551280e73c33b715
victor chin <victor@objectmage.com>
2024-08-17 20:11:56 -0400

    Completed Dictionary

---
+++ LeetCode/Dictionary/ContainsDuplicateII.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ContainsDuplicateII
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            if (k==0) return false;
+            HashSet<int> set = new(k);
+            for( int i=0; i<nums.Length;i++) { 
+                if (set.TryGetValue(nums[i], out _)) return true;
+                else
+                {
+                        if (i >= k) 
+                        { set.Remove(nums[i - k]); }
+                    
+                        set.Add(nums[i]);
+                }
+            }
+            return false;    
+        }
+    }
+}
---
+++ LeetCodeTests/DictionaryTest/ContainsDuplicateIITests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class ContainsDuplicateIITests
+    {
+        [TestMethod()]
+        public void ContainsNearbyDuplicateTest()
+        {
+            int[] nums = [1, 2, 3, 1];
+            int k = 3;
+            bool expected = true;
+            bool actual = new ContainsDuplicateII().ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ContainsNearbyDuplicateTest2()
+        {
+            int[] nums = [1, 0, 1, 1];
+            int k = 1;
+            bool expected = true;
+            bool actual = new ContainsDuplicateII().ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ContainsNearbyDuplicateTest3()
+        {
+            int[] nums = [1, 2, 3, 1, 2, 3];
+            int k = 2;
+            bool expected = false;
+            bool actual = new ContainsDuplicateII().ContainsNearbyDuplicate(nums, k);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
