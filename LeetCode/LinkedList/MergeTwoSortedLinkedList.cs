07b8e0031b1bf0955b223b7f4a6a77b28b8a7dc2
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-20 10:33:21 -0700

    debugger exercise

--- LeetCode/LinkedList/MergeTwoSortedLinkedList.cs
+++ LeetCode/LinkedList/MergeTwoSortedLinkedList.cs
@@ -8,41 +8,30 @@ namespace LeetCode
 {//https://leetcode.com/problems/merge-two-sorted-lists/
     public class MergeTwoListsLeet
     {
-        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+       public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1==null) return list2;
+        if (list2==null) return list1;
+
+        ListNode newHead =new ListNode(-1);
+        ListNode node= new ListNode(-1);
+        newHead.next = node;
+  
+        while (list1.next!=null &&list2.next!=null)//there is still nodes left
         {
-            if (list1 == null) return list2;
-            if (list2 == null) return list1;
-            ListNode resultHead=new ListNode(-1);
-            ListNode currentNode = resultHead;
-            while (list1 != null && list2 != null)
+            if (list1.next==null){node = list1; node.next = list2; break;}
+            if (list2.next==null){node = list2; node.next = list1; break;}
+            if (list1.val < list2.val)
             {
-
-                if (list1.val <= list2.val)
-                {
-                    currentNode.next = list1;
-                    list1 = list1.next!;
-
-                }
-                else
-                {
-                    currentNode.next = list2;
-                    list2 = list2.next!;
-                }
-                currentNode = currentNode.next;
-
-
-            }
-            if (list1 == null)
+                node = list1;
+                list1 = list1.next;
+            }else
             {
-                currentNode.next = list2;
-                return resultHead.next!;
+                node = list2;
+                list2 = list2.next;
             }
-            else
-            {
-                currentNode.next = list1;
-                return resultHead.next;
-            }
-            
+           node=node.next;
         }
+        return newHead.next;
+    }
     }
 }
