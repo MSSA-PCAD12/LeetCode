8a9a8019210e481e452676228f9db58e44a3f51e
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-21 13:44:59 -0700

    Add MergeSort

---
+++ LeetCode/Sorting/MergeSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MergeSort
+    {
+        public int[] Sort(int[] input) {
+            return DoMergeSort(input);
+        }
+
+        private int[] DoMergeSort(int[] input)
+        {
+            if (input.Length == 1) { return input; }
+
+            int mid = input.Length / 2;
+            var left= DoMergeSort(input[..mid]);
+            var right = DoMergeSort(input[mid..]);
+            int[] result=new int[(left.Length+right.Length)];
+            int idxLeft=0, idxRight=0,idxResult=0;
+            while (idxLeft < left.Length && idxRight < right.Length)
+            {
+                result[idxResult++]= left[idxLeft]<right[idxRight]? left[idxLeft++]:right[idxRight++];
+            }
+            if (idxLeft==left.Length) 
+            { Array.Copy(right,idxRight, result,idxResult,result.Length - idxResult); }//left side is done
+            if (idxRight == right.Length) 
+            { Array.Copy(left, idxLeft, result, idxResult, result.Length - idxResult); }//right side is done
+            return result;
+        }
+    }
+}
+
---
+++ LeetCodeTests/Sorting/MergeSortTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class MergeSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+        
+            System.Random rnd = new System.Random();
+            var input = Enumerable.Range(1, 10).OrderBy(x => rnd.Next(10)).ToArray();
+            var actual =  new MergeSort().Sort(input);
+            Array.Sort(input);
+            var expected = input;
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void SortTest2()
+        {
+
+            System.Random rnd = new System.Random();
+            var input = Enumerable.Range(1, 999).OrderBy(x => rnd.Next(1000)).ToArray();
+            var actual = new MergeSort().Sort(input);
+            Array.Sort(input);
+            var expected = input;
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void SortTest3()
+        {
+
+            System.Random rnd = new System.Random();
+            var input = Enumerable.Range(1, 9999).OrderBy(x => rnd.Next(10000)).ToArray();
+            var actual = new MergeSort().Sort(input);
+            Array.Sort(input);
+            var expected = input;
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+    }
+}
