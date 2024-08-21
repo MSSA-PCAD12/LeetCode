6e3dce561527d0ba950aea0cff6bb9a2443a52e0
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-21 13:58:40 -0700

    refactor add comment add Generic

--- LeetCode/Sorting/MergeSort.cs
+++ LeetCode/Sorting/MergeSort.cs
@@ -8,27 +8,71 @@ namespace LeetCode
 {
     public class MergeSort
     {
-        public int[] Sort(int[] input) {
-            return DoMergeSort(input);
-        }
 
-        private int[] DoMergeSort(int[] input)
+        public int[] DoMergeSort(int[] input)
         {
             if (input.Length == 1) { return input; }
 
-            int mid = input.Length / 2;
-            var left= DoMergeSort(input[..mid]);
-            var right = DoMergeSort(input[mid..]);
-            int[] result=new int[(left.Length+right.Length)];
-            int idxLeft=0, idxRight=0,idxResult=0;
-            while (idxLeft < left.Length && idxRight < right.Length)
+            int mid = input.Length / 2; //divide input to half
+            var left= DoMergeSort(input[..mid]); //send left half
+            var right = DoMergeSort(input[mid..]);//send right half
+            int[] result=new int[(left.Length+right.Length)]; //initialize result array
+            int idxLeft=0, idxRight=0,idxResult=0; //while loop index
+            while (idxLeft < left.Length && idxRight < right.Length) //do while both array has elements
             {
-                result[idxResult++]= left[idxLeft]<right[idxRight]? left[idxLeft++]:right[idxRight++];
+                result[idxResult++] = left[idxLeft]<right[idxRight]? left[idxLeft++]:right[idxRight++];
+                //pick the smaller number between left and right array and assign it to result
+                //increment idxResult index and left or right index
             }
-            if (idxLeft==left.Length) 
-            { Array.Copy(right,idxRight, result,idxResult,result.Length - idxResult); }//left side is done
-            if (idxRight == right.Length) 
-            { Array.Copy(left, idxLeft, result, idxResult, result.Length - idxResult); }//right side is done
+            if (idxLeft == left.Length) //the left side is done
+            {
+                Array.Copy(
+                right, //src array
+                idxRight,//src index to start copy
+                result, // destination array
+                idxResult,// destination array index to start pasting
+                result.Length - idxResult);//number of elements to copy
+            } //
+                            
+            //copy the remainder of right and append to unused result elements
+            //Array.Copy(src,srcIndex,dest,destIndex,length)
+            if (idxRight == right.Length) //the rigth side is done
+            { Array.Copy(left, idxLeft, result, idxResult, result.Length - idxResult); }
+            
+            return result;
+        }
+
+
+        public T[] DoMergeSort<T>(T[] input) where T:IComparable
+        {
+            if (input.Length == 1) { return input; }
+
+            int mid = input.Length / 2; //divide input to half
+            var left = DoMergeSort(input[..mid]); //send left half
+            var right = DoMergeSort(input[mid..]);//send right half
+            T[] result = new T[(left.Length + right.Length)]; //initialize result array
+            int idxLeft = 0, idxRight = 0, idxResult = 0; //while loop index
+            while (idxLeft < left.Length && idxRight < right.Length) //do while both array has elements
+            {
+                result[idxResult++] = left[idxLeft].CompareTo(right[idxRight])<=0 ? left[idxLeft++] : right[idxRight++];
+                //pick the smaller number between left and right array and assign it to result
+                //increment idxResult index and left or right index
+            }
+            if (idxLeft == left.Length) //the left side is done
+            {
+                Array.Copy(
+                right, //src array
+                idxRight,//src index to start copy
+                result, // destination array
+                idxResult,// destination array index to start pasting
+                result.Length - idxResult);//number of elements to copy
+            } //
+
+            //copy the remainder of right and append to unused result elements
+            //Array.Copy(src,srcIndex,dest,destIndex,length)
+            if (idxRight == right.Length) //the rigth side is done
+            { Array.Copy(left, idxLeft, result, idxResult, result.Length - idxResult); }
+
             return result;
         }
     }
--- LeetCodeTests/Sorting/MergeSortTests.cs
+++ LeetCodeTests/Sorting/MergeSortTests.cs
@@ -17,7 +17,7 @@ namespace LeetCode.Tests
         
             System.Random rnd = new System.Random();
             var input = Enumerable.Range(1, 10).OrderBy(x => rnd.Next(10)).ToArray();
-            var actual =  new MergeSort().Sort(input);
+            var actual =  new MergeSort().DoMergeSort(input);
             Array.Sort(input);
             var expected = input;
             CollectionAssert.AreEqual(expected, actual);
@@ -28,7 +28,7 @@ namespace LeetCode.Tests
 
             System.Random rnd = new System.Random();
             var input = Enumerable.Range(1, 999).OrderBy(x => rnd.Next(1000)).ToArray();
-            var actual = new MergeSort().Sort(input);
+            var actual = new MergeSort().DoMergeSort(input);
             Array.Sort(input);
             var expected = input;
             CollectionAssert.AreEqual(expected, actual);
@@ -39,7 +39,7 @@ namespace LeetCode.Tests
 
             System.Random rnd = new System.Random();
             var input = Enumerable.Range(1, 9999).OrderBy(x => rnd.Next(10000)).ToArray();
-            var actual = new MergeSort().Sort(input);
+            var actual = new MergeSort().DoMergeSort(input);
             Array.Sort(input);
             var expected = input;
             CollectionAssert.AreEqual(expected, actual);
