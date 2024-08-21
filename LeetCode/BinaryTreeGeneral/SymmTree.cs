dada8a507488c3cb8995b25f87d82726afb295d9
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-21 08:43:53 -0700

    SymmTree Test

---
+++ LeetCode/BinaryTreeGeneral/SymmTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.BinaryTreeGeneral
+{
+    public class SymmTree
+    {
+        public bool IsSymmetric(TreeNode root) {
+            if (root == null) return false;
+
+            return check(root.left,root.right);
+        }
+        bool check(TreeNode left, TreeNode right)
+        {
+            if (left == null && right==null)  return true;
+            if (left == null || right == null) return false;
+            return left.val == right.val && check(left.left, right.right) && check(left.right, right.left);
+
+        }
+
+        public bool IsSymmetric2(TreeNode root)
+        {
+            if (root == null) return false;
+            int level = 0;
+            bool isSymmetrical = false;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count != 0)
+            {
+                var node = stack.Pop();
+                if (node == null) continue;
+            }
+
+            return false;
+        }
+       
+    }
+}
---
+++ LeetCodeTests/BinaryTreeGeneralTest/IsSymmetricalTest.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Tests;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.BinaryTreeGeneral;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class IsSymmetricalTest
+    {
+        [TestMethod()]
+        public void SymmTreeTest1()
+        {
+            var input = BinaryTreeHelper.BuildTree([1, 2, 2, 3, 4, 4, 3]);
+            var expected = true;
+            var actual = new SymmTree().IsSymmetric(input!);
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void SymmTreeTest2()
+        {
+            var input = BinaryTreeHelper.BuildTree([1, 2, 2, null, 3, null, 3]);
+            var expected = false;
+            var actual = new SymmTree().IsSymmetric(input!);
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
