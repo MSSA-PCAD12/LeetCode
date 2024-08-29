74c0a64cc1a094243821adbf3200b7f0593bc412
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-29 08:45:35 -0700

    Add ReverseWordsInString Leetcode

---
+++ LeetCode/ArrayString/ReverseWordsInString.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.ArrayString
+{
+    //https://leetcode.com/problems/reverse-words-in-a-string
+    public class ReverseWordsInString
+    {
+        public string ReverseWords(string s)
+        {
+            Stack<string> words = new Stack<string>();
+
+            string output = "";
+            string wordBuilder = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ')
+                {
+                    wordBuilder += s[i];
+                }
+                else
+                {
+                    if (wordBuilder.Length > 0)
+                    {
+                        words.Push(wordBuilder);
+                        wordBuilder = "";
+                    }
+                }
+            }
+
+            if (wordBuilder.Length > 0)
+            {
+                words.Push(wordBuilder);
+            }
+
+            while (words.Count > 0)
+            {
+                output += words.Pop();
+
+                if (words.Count > 0)
+                {
+                    output += " ";
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ LeetCodeTests/ArrayString/ReverseWordsInStringTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.ArrayString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.ArrayString.Tests
+{
+    [TestClass()]
+    public class ReverseWordsInStringTests
+    {
+        [TestMethod()]
+        public void ReverseWordsTest()
+        {
+            string input = "the sky is blue";
+            string expected = "blue is sky the";
+            string actual = new ReverseWordsInString().ReverseWords(input);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ReverseWordsTest2()
+        {
+            string input = "  hello world  ";
+            string expected = "world hello";
+            string actual = new ReverseWordsInString().ReverseWords(input);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ReverseWordsTest3()
+        {
+            string input = "a good   example";
+            string expected = "example good a";
+            string actual = new ReverseWordsInString().ReverseWords(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
