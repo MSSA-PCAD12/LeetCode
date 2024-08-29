1457628e1451ae47e39e430adfff212b462e7e20
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-08-29 10:48:30 -0700

    CanCompleteCircute

---
+++ LeetCode/ArrayString/CanCompleteCircuit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.ArrayString
+{
+    public class CanCompleteCircuitLeet
+    {
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            if (gas.Sum() < cost.Sum()) return -1; //deal with test cases that has 99,999 1's
+
+            IEnumerable<int> candidates =
+            Enumerable.Range(0, gas.Length).Where(index =>
+            gas[index] != 0 && //any station with 0 gas is not a candidate
+            gas[index] >= cost[index] //any station with less gas than cost to next station is not a candidate
+            );
+
+            foreach (int station in candidates)
+            {
+                int startingStation = station;
+                int currentStation = station;
+                int gasLevel = gas[currentStation];
+                while (gasLevel - cost[currentStation] >= 0)
+                {
+                    gasLevel -= cost[currentStation];
+                    currentStation++;
+                    currentStation = currentStation % gas.Length;
+
+                    gasLevel += gas[currentStation];
+                    if (currentStation == startingStation) return startingStation;
+                };
+            }
+            return -1;
+        }
+    }
+}
---
+++ LeetCodeTests/ArrayString/CanCompleteCircuitLeetTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.ArrayString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.ArrayString.Tests
+{
+    [TestClass()]
+    public class CanCompleteCircuitLeetTests
+    {
+        [TestMethod()]
+        public void CanCompleteCircuitTest()
+        {
+            int[] gas = [5, 1, 2, 3, 4];
+            int[] cost = [4, 4, 1, 5, 1];
+            int expected = 4;
+            int actual = new CanCompleteCircuitLeet().CanCompleteCircuit(gas, cost);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void CanCompleteCircuitTest2()
+        {
+            int[] gas = [5, 1, 2, 3, 4];
+            int[] cost = [4, 4, 1, 5, 1];
+            int expected = 4;
+            int actual = new CanCompleteCircuitLeet().CanCompleteCircuit(gas, cost);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
