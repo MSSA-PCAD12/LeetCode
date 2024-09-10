816f144e01a96d78ebb00e012c79dd0675e3b970
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-09-10 08:38:13 -0700

    validating sudoku

---
+++ LeetCodeTests/Matrix/SudokuTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Matrix;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Matrix.Tests
+{
+    [TestClass()]
+    public class SudokuTests
+    {
+        [TestMethod()]
+        public void IsValidSudokuTest()
+        {
+            char[][] input = [['5', '3', '.', '.', '7', '.', '.', '.', '.'], ['6', '.', '.', '1', '9', '5', '.', '.', '.'], ['.', '9', '8', '.', '.', '.', '.', '6', '.'], ['8', '.', '.', '.', '6', '.', '.', '.', '3'], ['4', '.', '.', '8', '.', '3', '.', '.', '1'], ['7', '.', '.', '.', '2', '.', '.', '.', '6'], ['.', '6', '.', '.', '.', '.', '2', '8', '.'], ['.', '.', '.', '4', '1', '9', '.', '.', '5'], ['.', '.', '.', '.', '8', '.', '.', '7', '9']];
+            bool expected = true;
+            var actual = new Sudoku().IsValidSudoku(input);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void IsValidSudokuTest2()
+        {
+            char[][] input = [['8', '3', '.', '.', '7', '.', '.', '.', '.'], ['6', '.', '.', '1', '9', '5', '.', '.', '.'], ['.', '9', '8', '.', '.', '.', '.', '6', '.'], ['8', '.', '.', '.', '6', '.', '.', '.', '3'], ['4', '.', '.', '8', '.', '3', '.', '.', '1'], ['7', '.', '.', '.', '2', '.', '.', '.', '6'], ['.', '6', '.', '.', '.', '.', '2', '8', '.'], ['.', '.', '.', '4', '1', '9', '.', '.', '5'], ['.', '.', '.', '.', '8', '.', '.', '7', '9']];
+            bool expected = false;
+            var actual = new Sudoku().IsValidSudoku(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
