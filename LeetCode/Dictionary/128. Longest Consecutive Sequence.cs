134bba737aeacac3d8538e9255565562ace50a86
Victor Chin <116740251+Chinjila@users.noreply.github.com>
2024-09-12 09:56:06 -0700

    Interval Leet

---
+++ LeetCode/Dictionary/128. Longest Consecutive Sequence.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Dictionary
+{
+    public class Consecutive
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            nums = nums.Distinct().OrderBy(n=>n).ToArray();
+            Array.Sort(nums);
+
+            Dictionary<int, int> group = new();
+            for (int i = 0; i < nums.Length; i++) {
+                if (group.ContainsKey(i - nums[i]))
+                {
+                    group[i - nums[i]] += 1;
+                }
+                else
+                {
+                    group.Add(i-nums[i], 1);
+                }
+            
+            }
+          
+            return group.Max(g=>g.Value);
+        }
+
+
+        public int LongestConsecutive2(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            HashSet<int> hashset = new HashSet<int>(nums);
+            int currentNum = 0;
+            int currConsecutive = 0;
+            int maxConsecutive = int.MinValue;
+            foreach (int i in hashset) {
+                currentNum = i;
+                if (!hashset.Contains(currentNum - 1))
+                {
+                    currentNum++;
+                    currConsecutive = 1;
+                    while (hashset.Contains(currentNum))
+                    {
+                        currentNum++;
+                        currConsecutive++;
+
+                    }
+                    maxConsecutive = Math.Max(maxConsecutive, currConsecutive);
+                }
+
+                
+            }
+
+            return maxConsecutive;
+        }
+    }
+}
